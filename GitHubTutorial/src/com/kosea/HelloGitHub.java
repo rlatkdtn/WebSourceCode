d05a69e21467d75ebc67d1fdce19427b8642fef9
Administrator <Administrator@0-pc>
2018-07-04 14:58:39 +0900

    세번째 업로드

--- GitHubTutorial/src/com/kosea/HelloGitHub.java
+++ GitHubTutorial/src/com/kosea/HelloGitHub.java
@@ -3,6 +3,11 @@ package com.kosea;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
+	
+		for(int i=1; i<=10; i++)
+			System.out.print("^");
+		
+		System.out.println("");
 		
 		System.out.println("Hello! GitHub");
 		
